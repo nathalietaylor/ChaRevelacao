54911aab6162786aa1b448ea3afc5bf931150a79
Nathalie Taylor <nmtzampieri@gmail.com>
2025-04-10 22:52:11 -0300

    Intro2

--- cha-revelacao/src/pages/Intro2.jsx
+++ cha-revelacao/src/pages/Intro2.jsx
@@ -1,19 +1,34 @@
+// src/pages/Intro2.jsx
 import { useNavigate } from "react-router-dom";
+import "../styles/Intro2.css";
 
 export default function Intro2() {
   const navigate = useNavigate();
 
   return (
-    <div className="janela">
-      <h2>🚫 Não, não estou grávida.</h2>
-      <p>
-        Passei por meses em um processo de diagnóstico e agora você descobrirá
-        se sou só estranha ou se vim com defeito de fábrica.
-      </p>
-      <p>Boa sorte!</p>
-      <div>
-        <button onClick={() => navigate("/resposta/autista")}>Autista</button>
-        <button onClick={() => navigate("/resposta/estranha")}>Estranha</button>
+    <div className="container">
+      <div className="popup">
+        <div className="popup-header">
+          <span className="title">Aviso</span>
+        </div>
+        <div className="popup-body">
+          <p>
+            Não, eu não estou grávida. <br />
+            Passei por meses em um processo de diagnóstico e agora...
+            <br />
+            Você vai descobrir: <br />
+            Sou só estranha ou vim com defeito de fábrica? 😅
+          </p>
+          <p>Boa sorte!</p>
+          <div className="buttons">
+            <button onClick={() => navigate("/resposta/estranha")}>
+              Estranha
+            </button>
+            <button onClick={() => navigate("/resposta/autista")}>
+              Autista
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
